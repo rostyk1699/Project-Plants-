2e4f7f2c3ba403cd3f371460ca28876ffbc9b936
Rostyslav <rostkost77@gmail.com>
2024-06-07 04:37:48 +0200

    Ðadd class Flower which inherits class Plant

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ private:
     double height;
 public:
     Plant(const string &name, const string &species, int age, double height): name(name), species(species), age(age), height(height) {}
-
+    virtual ~Plant() {}
     string getName() const { return name; }
     string getSpecies() const { return species; }
     int getAge() const { return age; }
@@ -22,11 +22,24 @@ public:
     void setAge(int age) { this->age = age; }
     void setHeight(double height) { this->height = height; }
 
-    void displayInfo() const {
-        cout << "Name: " << name << ", Species: " << species
-             << ", Age: " << age << " years, Height: " << height << " cm" << endl;
+    virtual void displayInfo() const {
+        cout << "Name: " << name << ", Species: " << species << ", Age: " << age << " years, Height: " << height << " cm" << endl;
+    }
+
+};
+class Flower : public Plant {
+private:
+    string color;
+
+public:
+    Flower(const string &name, const string &species, int age, double height, const string &color): Plant(name, species, age, height), color(color) {}
+
+    void displayInfo() const override {
+        Plant::displayInfo();
+        cout << "Color: " << color << endl;
     }
 };
+
 class PlantManager {
 private:
     vector<Plant> plants;
@@ -34,6 +47,7 @@ public:
     void addPlant(const Plant &plant) {
         plants.push_back(plant);
     }
+
     void displayAllPlants() const {
         for (const auto &plant : plants) {
             plant.displayInfo();
@@ -55,14 +69,17 @@ public:
 int main() {
     PlantManager manager;
     int choice;
+
     while (true) {
         cout << "1. Add Plant\n2. Display All Plants\n3. Search Plant by Name\n4. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
+
         if (choice == 1) {
             string name, species;
             int age;
             double height;
+
             cout << "Enter plant name: ";
             cin >> name;
             cout << "Enter plant species: ";
@@ -71,20 +88,27 @@ int main() {
             cin >> age;
             cout << "Enter plant height (cm): ";
             cin >> height;
+
             Plant newPlant(name, species, age, height);
             manager.addPlant(newPlant);
+
         } else if (choice == 2) {
             manager.displayAllPlants();
+
         } else if (choice == 3) {
             string name;
             cout << "Enter plant name to search: ";
             cin >> name;
             manager.searchPlantByName(name);
+
         } else if (choice == 4) {
             break;
+
         } else {
             cout << "Invalid choice. Please try again." << endl;
         }
     }
+
     return 0;
 }
+
